53f9ab71ae90a6521768d336695cc4fec2324c50
Rishi <khatririshi2430@gmail.com>
2020-11-25 15:39:07 +0545

    Schema added for the dashboard

--- apps/task/schema.py
+++ apps/task/schema.py
@@ -2,7 +2,7 @@ import datetime
 from dateutil.relativedelta import relativedelta
 
 
-from django.db.models import F, Sum, DurationField
+from django.db.models import F, Sum
 from django.utils.timezone import now
 
 import graphene
@@ -19,7 +19,6 @@ from usergroup.schema import UserGroupType
 from task.models import TaskGroup, Task, TimeEntry
 from task.enums import StatusGrapheneEnum
 from task.filters import TaskFilter, TaskGroupFilter, TimeEntryFilter
-from project.schema import ProjectType
 from project.models import Project
 
 
@@ -82,6 +81,19 @@ class SummaryDay(graphene.ObjectType):
         return queryset
 
 
+class ProjectDetail(graphene.ObjectType):
+    duration = graphene.String()
+    project_name = graphene.String()
+
+
+class DashBoardMyProject(graphene.ObjectType):
+    project_name = graphene.String()
+    client_name = graphene.String()
+    edited_on = graphene.Date()
+    status = graphene.String()
+    hours_spent = graphene.String()
+
+
 class SummaryWeekType(graphene.ObjectType):
     total_hours_weekly = graphene.String()
     total_hours_day = graphene.List(SummaryDay)
@@ -97,32 +109,122 @@ class SummaryWeekDashBoard(graphene.ObjectType):
     total_hours_day = graphene.List(SummaryDay)
 
 
+class SummaryProjectDashBoard(graphene.ObjectType):
+    project_total = graphene.String()
+    project_particular = graphene.List(ProjectDetail)
+
+
+class SummaryMostActiveProject(graphene.ObjectType):
+    project_total = graphene.String()
+    project_particular = graphene.List(ProjectDetail)
+
+
 class DashBoardType(graphene.ObjectType):
     this_week = graphene.Field(SummaryWeekDashBoard)
-    """project_total = graphene.String()
-    project_individual = graphene.List(ProjectType)"""
+    hours_by_project = graphene.Field(SummaryProjectDashBoard)
+    most_active_project = graphene.Field(SummaryMostActiveProject)
+    my_project = graphene.List(DashBoardMyProject)
 
-    def resolve_this_week(root, info, *a, **k):
+    def resolve_this_week(root, info, *args, **kwargs):
         date = datetime.date.today()
         start_week = date - datetime.timedelta(date.weekday())
         end_week = start_week + datetime.timedelta(6)
         user = info.context.user
-        root = TimeEntry.objects.filter(
+        queryset= TimeEntry.objects.filter(
             user=info.context.user,
             date__range=[start_week, end_week]
         ).order_by().values('date')
         if user.is_authenticated:
             # week total duration
-            hours_week = root.annotate(
+            hours_week = queryset.annotate(
                 duration=F('end_time') - F('start_time'),
+            ).aggregate(sum=Sum('duration'))['sum']
+
+            hours_day = queryset.annotate(
+                duration=Sum(F('end_time') - F('start_time'))
             )
             return SummaryWeekDashBoard(
-                total_hours=hours_week.aggregate(sum=Sum('duration'))['sum'],
-                total_hours_day=hours_week
+                total_hours=hours_week,
+                total_hours_day=hours_day
             )
         else:
             return None
 
+    def resolve_hours_by_project(root, info, *args, **kwargs):
+        user = info.context.user
+        if not user:
+            return None
+        else:
+            # get the project for the user
+            projects = Project.get_for(user)
+            print('project', projects)
+            queryset = TimeEntry.objects.filter(
+                user=user,
+                task__task_group__project__in=projects
+            )
+            total_project_hours = queryset.annotate(
+                duration=F('end_time') - F('start_time'),
+            ).aggregate(sum=Sum('duration'))['sum']
+
+            each_project_hours = queryset.order_by().values('task__task_group__project').annotate(
+                duration=Sum(F('end_time') - F('start_time')),
+            ).values('duration', project_name=F('task__task_group__project__title'))
+
+            return SummaryProjectDashBoard(
+                project_total=total_project_hours,
+                project_particular=each_project_hours
+            )
+
+    def resolve_most_active_project(root, info, *args, **kwargs):
+        # gives the most active project in the week
+        date = datetime.date.today()
+        start_week = date - datetime.timedelta(date.weekday())
+        end_week = start_week + datetime.timedelta(6)
+        user = info.context.user
+        if not user:
+            return None
+        else:
+            # get the project for the user
+            projects = Project.get_for(user)
+            queryset = TimeEntry.objects.filter(
+                user=user,
+                task__task_group__project__in=projects,
+                date__lte=end_week,
+                date__gte=start_week
+            )
+            total_project_hours = queryset.annotate(
+                duration=F('end_time') - F('start_time'),
+            ).aggregate(sum=Sum('duration'))['sum']
+
+            each_project_hours = queryset.order_by().values('task__task_group__project').annotate(
+                duration=Sum(F('end_time') - F('start_time')),
+            ).values('duration', project_name=F('task__task_group__project__title'))
+
+            return SummaryMostActiveProject(
+                project_total=total_project_hours,
+                project_particular=each_project_hours
+            )
+
+    def resolve_my_project(root, info, *args, **kwargs):
+        user = info.context.user
+        if not user:
+            return None
+        else:
+            # get the projects for the user
+            projects = Project.get_for(user=user)
+            queryset = TimeEntry.objects.filter(
+                task__task_group__project__in=projects
+            ).order_by().values('task__task_group__project').annotate(
+                duration=Sum(F('end_time') - F('start_time'))
+            ).values(
+                hours_spent=F('duration'),
+                project_name=F('task__task_group__project__title'),
+                client_name=F('task__task_group__project__client__name'),
+                edited_on=F('task__task_group__project__modified_at'),
+                status=F('task__task_group__status')
+                )
+            return queryset
+            
 
 class Query(object):
     taskgroup = graphene.Field(TaskGroupType)
@@ -211,13 +313,4 @@ class Query(object):
 
     # TODO: Remove one
     def resolve_dashboard(root, info, **kwargs):
-        return DashBoardType()
-
-    def resolve_dashboard_1(root, info, **kwargs):
-        date = datetime.date.today()
-        start_week = date - datetime.timedelta(date.weekday())
-        end_week = start_week + datetime.timedelta(6)
-        return TimeEntry.objects.filter(
-                user=info.context.user,
-                date__range=[start_week, end_week]
-            ).order_by().values('date')
+            return DashBoardType()
